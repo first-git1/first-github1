7d210b988090a250e0ebc88939621e0c70172811
sgw <1098017281@qq.com>
2016-11-25 08:24:14 +0800

    add first jaca

---
+++ ts/src/ts/MyTs.java
@@ -0,0 +1,26 @@
+package ts;
+
+public class MyTs {
+public String name;
+public String addres;
+public String phone;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddres() {
+	return addres;
+}
+public void setAddres(String addres) {
+	this.addres = addres;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+}
